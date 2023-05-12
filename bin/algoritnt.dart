3c1f3255af5fd9415d4f88bcf9ef1b2f3a6bbb5f
Berlinkg <nadyrbekov570@gmail.com>
2023-05-12 23:49:52 +0600

    algoritm

---
+++ bin/algoritnt.dart
@@ -0,0 +1,5 @@
+import 'package:algoritnt/algoritnt.dart' as algoritnt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${algoritnt.calculate()}!');
+}
---
+++ test/algoritnt_test.dart
@@ -0,0 +1,8 @@
+import 'package:algoritnt/algoritnt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
